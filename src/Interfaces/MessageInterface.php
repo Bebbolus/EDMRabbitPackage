53ef54149335b42adfacc82f4d3918f96cffd50f
ROBERTO <lambis.net@gmail.com>
2017-05-12 14:50:54 +0200

    decoplued a little

--- src/Interfaces/MessageInterface.php
+++ src/Interfaces/MessageInterface.php
@@ -6,45 +6,34 @@ use EDMRabbitPackage\Connectors\RabbitConnector;
 
 class MessageInterface
 {
-    private $index;
-    private $type;
-    private $action;
-    private $request;
-    private $user;
+    private $body;
+    private $queue;
 
     public function __construct($index, $type, $action, $request, $user)
     {
-        $this->index = $index;
-        $this->type = $type;
-        $this->action = $action;
-        $this->request = $request;
-        $this->user = $user;
+        $this->body =
+            [
+                'index'     => $index,
+                'type'      => $type,
+                'action'    => $action,
+                'request'   => $request,
+                'user'      => $user
+            ];
+        $this->queue = $type;
     }
 
 
     public function sendWithResponse()
     {
         $connector = new RabbitConnector();
-        $corrId = $connector->sendMessageWithResponse([
-            'index' => $this->index,
-            'type' => $this->type,
-            'action' => $this->action,
-            'request' => $this->request,
-            'user' => $this->user
-        ], "document");
+        $corrId = $connector->sendMessageWithResponse($this->body, $this->queue);
         return $connector->consumeQueueFromCorrelationId($corrId);
     }
 
     public function send()
     {
         $connector = new RabbitConnector();
-        $connector->sendMessage([
-            'index' => $this->index,
-            'type' => $this->type,
-            'action' => $this->action,
-            'request' => $this->request,
-            'user' => $this->user
-        ], "document");
+        $connector->sendMessage($this->body, $this->queue);
         return true;
     }
 
